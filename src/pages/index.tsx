013a7e9af72229d04c7eaa6696667d68e8056394
Ruan Pablo <ruan_goio10@hotmail.com>
2022-03-16 14:09:00 -0300

    chakra config

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+
+export default function Home() {
+
+
+    return (
+
+        <>
+            <Head>
+                <title>Home | dashGo</title>
+            </Head>
+            <h1>😎</h1>
+        </>
+    )
+}
