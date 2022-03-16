2fcdccbf1a36e6fc9aab2d8a6a246bb5ca92129c
Ruan Pablo <ruan_goio10@hotmail.com>
2022-03-16 15:16:38 -0300

    Login Screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
 
 
 export default function Home() {
@@ -11,7 +11,47 @@ export default function Home() {
             <Head>
                 <title>Home | dashGo</title>
             </Head>
-            <h1>Hello World 😎</h1>
+
+            <Flex w="100vw" h="100vh" align="center" justify="center">
+                <Flex as="form" w="100%" maxW="360px" bg="gray.800" p="8" borderRadius={9} flexDir="column">
+                    <Stack mt={4}>
+                        <FormControl>
+                            <FormLabel htmlFor='email'>E-mail</FormLabel>
+                            <Input
+                                id='email'
+                                name="email"
+                                type="email"
+                                focusBorderColor="pink.500"
+                                bgColor="gray.900"
+                                variant="filled"
+                                _hover={{
+                                    bgColor: 'gray.900'
+                                }}
+                                size="lg"
+                            />
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel htmlFor='password'>Senha</FormLabel>
+                            <Input
+                                id='password'
+                                name="password"
+                                type="password"
+                                focusBorderColor="pink.500"
+                                bgColor="gray.900"
+                                variant="filled"
+                                _hover={{
+                                    bgColor: 'gray.900'
+                                }}
+                                size="lg"
+                            />
+                        </FormControl>
+                    </Stack>
+
+                    <Button type='submit' mt={6} colorScheme='pink' size="lg" >Entrar</Button>
+                </Flex>
+            </Flex>
+
         </>
     )
 }
