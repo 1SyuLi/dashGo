54801e4979cad880834d9de90673b21de7fbab6c
Ruan Pablo <ruan_goio10@hotmail.com>
2022-03-18 18:51:30 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
 
 
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right" >
-                <Text>Ruan Pablo</Text>
-                <Text color="gray.300" fontSize="small" >
-                    ruangoio01@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right" >
+
+                    <Text>Ruan Pablo</Text>
+                    <Text color="gray.300" fontSize="small" >
+                        ruangoio01@gmail.com
+                    </Text>
+                </Box>
+            )}
+
 
             <Avatar size="md" name="Ruan Pablo" src="https://i.pinimg.com/originals/9f/16/d9/9f16d926f95e7ccc80b3771c50edaec3.gif" />
         </Flex>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -8,16 +8,20 @@ import { SearchBox } from './SearchBox';
 
 export function Header() {
 
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    });
 
     return (
         <Flex as="header" w="100%" maxW={1280} h="20" mx="auto" mt="4" align="center" px="6">
             <Logo />
 
-            <SearchBox />
+            {isWideVersion && <SearchBox />}
 
             <Flex align="center" ml="auto">
                 <NotificationsNav />
-                <Profile />
+                <Profile showProfileData={isWideVersion} />
             </Flex>
         </Flex>
     )
