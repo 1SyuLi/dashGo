dbecbdfa860966c831f81df58866e076eed38f32
Ruan Pablo <ruan_goio10@hotmail.com>
2022-03-18 22:26:03 -0300

    Responsive Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -8,6 +10,8 @@ import { SearchBox } from './SearchBox';
 
 export function Header() {
 
+    const { onOpen } = useSidebarDrawer();
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -15,6 +19,17 @@ export function Header() {
 
     return (
         <Flex as="header" w="100%" maxW={1280} h="20" mx="auto" mt="4" align="center" px="6">
+            {!isWideVersion && (
+                <IconButton
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    aria-label={'open-navigation'}
+                    mr={2}
+                />
+            )}
+
             <Logo />
 
             {isWideVersion && <SearchBox />}
