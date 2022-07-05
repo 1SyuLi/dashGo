e4786796a71510274a73d4ffa04efcdf94e144c8
Ruan Pablo <ruan_goio10@hotmail.com>
2022-07-05 18:06:03 -0300

    staleTime

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -30,7 +30,23 @@ export default function UserList() {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users');
         const data = await response.json();
-        return data;
+
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                }),
+            }
+        })
+
+        return users;
+    }, {
+        staleTime: 1000 * 5,
     });
 
     const isWideVersion = useBreakpointValue({
@@ -86,27 +102,29 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Ruan Pablo</Text>
-                                                <Text fontSize="small" color="gray.300">
-                                                    ruangoio01@gmail.com
-                                                </Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>17 de Março, 2022</Td>}
-                                        <Td>
-                                            {!isWideVersion &&
-                                                <Button cursor="pointer" as="a" size="sm" fontSize="small" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                                    Editar
-                                                </Button>
-                                            }
-                                        </Td>
-                                    </Tr>
+                                    {data.map(user => (
+                                        <Tr key={user.id}>
+                                            <Td px={["4", "4", "6"]}>
+                                                <Checkbox colorScheme="pink" />
+                                            </Td>
+                                            <Td>
+                                                <Box>
+                                                    <Text fontWeight="bold">{user.name}</Text>
+                                                    <Text fontSize="small" color="gray.300">
+                                                        {user.email}
+                                                    </Text>
+                                                </Box>
+                                            </Td>
+                                            {isWideVersion && <Td>{user.createdAt}</Td>}
+                                            <Td>
+                                                {!isWideVersion &&
+                                                    <Button cursor="pointer" as="a" size="sm" fontSize="small" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                        Editar
+                                                    </Button>
+                                                }
+                                            </Td>
+                                        </Tr>
+                                    ))}
                                 </Tbody>
                             </Table>
 
